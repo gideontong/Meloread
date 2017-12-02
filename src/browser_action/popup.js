65262b5361f1ee3c21216a32ef6da4510813943d
Gideon Tong <funnygid@gmail.com>
2017-12-02 02:47:43 -0800

    Working Watson API!

--- src/browser_action/popup.js
+++ src/browser_action/popup.js
@@ -9,10 +9,12 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
 	}
 	
 	var parsedText = strip(request.source);
-	message.innerText = parsedText;
+	//message.innerText = parsedText.replace(/(\r\n|\n|\r)/gm," ").replace(/['"]+/g," ");
 	// To-Do: Strip inline CSS
 	
-	function getTone(text) {
+	//* THIS DOESN'T WORK. I DON'T KNOW WHY, BUT IT ALSO BREAKS HTML.
+	 
+	/* function getTone(text) {
 		var ToneAnalyzerV3 = require('https://gateway.watsonplatform.net/tone-analyzer/api');
 		var tone_analyzer = new ToneAnalyzerV3({
 		 username: '',
@@ -26,15 +28,58 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
 		};
 		
 		tone_analyzer.tone(params, function(error, response) {
-			if (error)
+			if (error).
 				console.log("Error found at: ", error);
 			else
-				console.log("JSON.stringify(response, null, 2));
+				console.log(JSON.stringify(response, null, 2));
+		})
+	} */
+	function processTone(mydata){
+		tonescare = ["disgust", "anger", "fear", "joy", "sadness"];
+		var myDocTone;
+		var myDocToneScore = 0;
+		for (pretone in mydata["document_tone"]["tones"]) {
+			tone = mydata["document_tone"]["tones"][pretone];
+			console.log(tone);
+			if (tonescare.indexOf(tone["tone_id"]) != -1
+			//$.inArray(tone["tone_id"], tonescare)
+			) {
+				if (tone["score"] > myDocToneScore) {
+					myDocToneScore = tone["score"];
+					myDocTone = tone["tone_id"];
+				}
+			}
+		}
+		
+		if (!myDocTone) {
+			// DO SOMETHING WHEN THERE IS NO TONE
+			no_tone.innerText = "No tone.";
+		} else {
+			no_tone.innerText = myDocTone;
 		}
 	}
 	
+	function getWatson(parsedText){
+		$.ajax("https://@gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2017-09-21", {
+			"data": JSON.stringify({"text": parsedText.replace(/(\r\n|\n|\r)/gm," ").replace(/['"]+/g," ")}),
+			"type": "post",
+			"processData": false,
+			"contentType": "application/json",
+			"success": function(data) {
+				//tone_answer.innerText = JSON.stringify(data);
+				processTone(data);
+			}
+		})
+	}
+	
+	mydata = getWatson(parsedText);
+	
+	/*
 	var parsedTone = getTone(parsedText);
 	tone_answer.innerText = parsedTone;
+	*/
+	
+	
   }
 });
 
